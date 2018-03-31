f7c26065c0d8e37350a48e3cf670571b58b3ac7e
Asad Sahi <asadazmat@gmail.com>
2018-03-31 19:40:11 +0100

    first commit after ng6 upgrade

---
+++ projects/client/src/app/shared/decorators/debug-log.decorator.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/do';
+
+export function log$(target: any, propertyKey: string) {
+  let propertyValue: any;
+
+  function getter() {
+    return propertyValue;
+  }
+
+  function setter(value: any) {
+    if (value instanceof Observable) {
+      propertyValue = value.do(res => {
+        const isArrayOfObjects = Array.isArray(res) && typeof res[0] === 'object';
+        const logType = isArrayOfObjects ? 'table' : 'log';
+        console.groupCollapsed(propertyKey);
+        console[logType](res);
+        console.groupEnd();
+      });
+    } else {
+      propertyValue = value;
+    }
+  }
+
+  Object.defineProperty(target, propertyKey, {
+    get: getter,
+    set: setter,
+    enumerable: true,
+    configurable: true
+  });
+}
