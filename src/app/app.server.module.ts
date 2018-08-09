98fb8d89349f4be91507936eb1bd1acc1ef60ae5
Asad Sahi <asadazmat@gmail.com>
2018-08-09 21:56:22 +0100

    app shell

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { ServerTransferStateModule } from '@angular/platform-server';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
 
 @NgModule({
     imports: [
@@ -12,7 +16,8 @@ import { ServerTransferStateModule } from '@angular/platform-server';
         NoopAnimationsModule,
         ServerTransferStateModule,
         ServerModule,
-        ModuleMapLoaderModule
+        ModuleMapLoaderModule,
+        RouterModule.forRoot(routes),
     ],
     bootstrap: [AppComponent],
     providers: [
