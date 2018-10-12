7f3ba10e14d0ef263af26a62cafffb2fec380dbe
Asad Sahi <asadazmat@gmail.com>
2018-10-12 21:50:48 +0100

    angular 7 rc

--- src/app/+examples/examples/crud-shop/customers/customers.component.spec.ts
+++ src/app/+examples/examples/crud-shop/customers/customers.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomersComponent } from './customers.component';
-import { CustomersService } from './customers.service';
 
 describe('CustomersComponent', () => {
   let component: CustomersComponent;
@@ -9,8 +8,7 @@ describe('CustomersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CustomersComponent],
-      providers: [CustomersService]
+      declarations: [CustomersComponent]
     })
       .compileComponents();
   }));
