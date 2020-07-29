70fc994856abfb5a9d6b2e7d99e1fcfac1783bef
Asad Sahi <asadazmat@gmail.com>
2020-07-29 14:11:54 +0100

    fix

--- src/app/shared/components/forms/components/field-base/field-base.ts
+++ src/app/shared/components/forms/components/field-base/field-base.ts
@@ -1,21 +1,19 @@
-import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Injectable, Directive } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
-import { IFieldConfig, Field } from '@app/models';
-import { AppFormComponent } from '../form';
-import { FormsService } from '../../forms.service';
+import { IFieldConfig, Field } from "@app/models";
+import { AppFormComponent } from "../form";
+import { FormsService } from "../../forms.service";
 
-@Injectable()
+@Directive()
 export abstract class FieldBaseComponent implements Field {
-    config: IFieldConfig;
-    constructor(
-        public fc: AppFormComponent,
-        private formService: FormsService) { }
-    get formGroup(): FormGroup {
-        return this.fc.form;
-    }
+  config: IFieldConfig;
+  constructor(public fc: AppFormComponent, private formService: FormsService) {}
+  get formGroup(): FormGroup {
+    return this.fc.form;
+  }
 
-    showAsterisk(config: IFieldConfig): boolean {
-        return this.formService.showIndicator(config);
-    }
+  showAsterisk(config: IFieldConfig): boolean {
+    return this.formService.showIndicator(config);
+  }
 }
